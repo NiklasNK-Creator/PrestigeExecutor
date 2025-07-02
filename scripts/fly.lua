3a6b3594808598c5acc2948b0b116f0d350d929e
NiklasNK-Creator <schoneniklas79@gmail.com>
2025-07-02 11:46:50 +0200

    Update fly.lua

--- scripts/fly.lua
+++ scripts/fly.lua
@@ -1,1 +1,12 @@
+-- Fly Script (basic example)
+local player = game.Players.LocalPlayer
+local mouse = player:GetMouse()
+local flying = false
+
+mouse.KeyDown:Connect(function(key)
+    if key == "f" then
+        flying = not flying
+        player.Character.HumanoidRootPart.Anchored = flying
+    end
+end)
 
